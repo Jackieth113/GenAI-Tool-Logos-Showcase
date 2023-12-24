45ccf5524c576dfbf290a18007c5d2cfcf6d9a85
Jackieth113 <154063147+Jackieth113@users.noreply.github.com>
2023-12-24 09:35:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-// JavaScript can be used for more advanced interactions if needed
-// Currently, the redirection is handled by the anchor tag in HTML
+function setSpeed(speedInSeconds) {
+    const logoContainer = document.getElementById('logo-container');
+    logoContainer.style.animationDuration = speedInSeconds + 's';
+}
+
+// Example: Set speed to 5 seconds
+setSpeed(5);
